b4cca0708ea592affb73299dac5385e90344ccb6
Rust <rustem.shakirov86@ya.ru>
2021-12-27 09:27:45 +0400

    added lesson2_1_step7.py

---
+++ lesson2_1_step7.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+link = "http://suninjuly.github.io/get_attribute.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_css_selector("img[src='images/chest.png']")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_xpath("//button[text()='Submit']")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
